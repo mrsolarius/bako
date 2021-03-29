7bb614d978117fed55642a75cd20ca7fc1a4b8bf
gimeneti <Tim.Gimenez@etu.univ-grenoble-alpes.fr>
2021-03-29 21:04:25 +0200

    BAKO-47 shared_preferences -> nbHeureCongesRestantes, nbHeureCongésRestantes, theme

---
+++ app/src/main/java/app/bako/view/navigation/CodeSettings.kt
@@ -0,0 +1,11 @@
+package app.bako.view.navigation
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class CodeSettings : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_code_settings)
+    }
+}
