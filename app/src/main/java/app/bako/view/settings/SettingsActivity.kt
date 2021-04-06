f48530efc4fa5d9eabe2d56398ff0f9d376d44b6
MrSolarius <45536184+mrsolarius@users.noreply.github.com>
2021-04-06 18:08:19 +0200

    BAKO-46 : Mise à jour des problémes

---
+++ app/src/main/java/app/bako/view/settings/SettingsActivity.kt
@@ -0,0 +1,62 @@
+package app.bako.view.settings
+
+import android.os.Bundle
+import android.view.MenuItem
+import android.view.View
+import androidx.appcompat.app.AppCompatActivity
+import androidx.preference.PreferenceFragmentCompat
+import app.bako.R
+import androidx.core.app.NavUtils
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentManager
+import androidx.preference.Preference
+import app.bako.view.navigation.MainActivity
+import app.bako.view.navigation.fragment.CodesListFragment
+import java.lang.reflect.Array.newInstance
+
+
+class SettingsActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.settings_activity)
+        supportFragmentManager
+            .beginTransaction()
+            .replace(R.id.settings, SettingsFragment())
+            .commit()
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        //Go back arrow btn
+        finish();
+        return true;
+    }
+
+    fun makeCurrentFragment(fragment: Fragment){
+        supportFragmentManager.beginTransaction().apply {
+            replace(R.id.fl_wrapper, fragment)
+            commit()
+        }
+    }
+
+    class SettingsFragment : PreferenceFragmentCompat() {
+        override fun onCreatePreferences(savedInstanceState: Bundle?, rootKey: String?) {
+            setPreferencesFromResource(R.xml.root_preferences, rootKey)
+        }
+
+        override fun onPreferenceTreeClick(preference: Preference?): Boolean {
+            val key = preference?.key
+            if (key.equals("startWorkCode")){
+                showSettingDialog()
+            }
+            return super.onPreferenceTreeClick(preference)
+        }
+
+        fun showSettingDialog(){
+            val fm: FragmentManager = this.parentFragmentManager
+            val editNameDialogFragment: CodesListFragment = CodesListFragment()
+            editNameDialogFragment.show(fm, "fragment_edit_name")
+        }
+    }
+}
